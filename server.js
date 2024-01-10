85392e31bd63909f8197da37b9f8670a3bca21b0
astin.biju <astin.biju@acsiatech.com>
2024-01-10 21:29:21 +0530

    delete firestore

--- server.js
+++ server.js
@@ -84,6 +84,17 @@ app.post('/update',async(req,res)=>{
 })
 
 
+app.delete('/delete/:id',async(req,res)=>{
+    try{
+        const response=db.collection("user").doc(req.params.id).delete();
+        res.send(response);
+    }catch(error)
+    {
+        res.send(error);
+    }
+})
+
+
 
 
 
